7551c8e07e2d3d721174bf17471d8405dd867988
Ángel <Angel>
2023-05-11 09:31:06 +0200

    Añado instalación y configuración de sddm y tema de qBittorrent

--- install.sh
+++ install.sh
@@ -5,21 +5,30 @@ set -eu
 dependencies() {
   if [ "${DISTRO:?}" = "Gentoo" ] || [ "${DISTRO:?}" = "gentoo" ]
   then
-    sudo emerge --ask dev-vcs/git media-libs/fontconfig x11-base/xorg-proto x11-libs/libX11 x11-libs/libXft x11-libs/libXinerama x11-terms/alacritty media-fonts/jetbrains-mono media-fonts/fontawesome media-gfx/feh x11-apps/xsetroot x11-apps/setxkbmap media-fonts/takao-fonts media-sound/pnmixer  gnome-extra/nm-applet sys-devel/gcc app-text/asciidoc dev-lang/python dev-libs/libconfig dev-libs/libev dev-libs/libpcre dev-libs/uthash dev-python/xcffib  dev-util/meson dev-util/meson-format-array dev-util/ninja sys-apps/dbus virtual/opengl virtual/pkgconfig x11-apps/xhost x11-base/xorg-server x11-libs/libXext x11-libs/libdrm x11-libs/libxcb x11-libs/pixman x11-libs/xcb-util-image x11-libs/xcb-util-renderutil x11-misc/dunst xfce-base/thunar xfce-extra/xarchiver xfce-extra/thunar-archive-plugin x11-misc/rofi x11-misc/stalonetray app-admin/doas x11-misc/cbatticon app-shells/dash lxde-base/lxappearance x11-misc/sddm net-p2p/qbittorrent > /dev/null
+    sudo emerge --ask dev-vcs/git media-libs/fontconfig x11-base/xorg-proto x11-libs/libX11 x11-libs/libXft x11-libs/libXinerama x11-terms/alacritty media-fonts/jetbrains-mono media-fonts/fontawesome media-gfx/feh x11-apps/xsetroot x11-apps/setxkbmap media-fonts/takao-fonts media-sound/pnmixer  gnome-extra/nm-applet sys-devel/gcc app-text/asciidoc dev-lang/python dev-libs/libconfig dev-libs/libev dev-libs/libpcre dev-libs/uthash dev-python/xcffib  dev-util/meson dev-util/meson-format-array dev-util/ninja sys-apps/dbus virtual/opengl virtual/pkgconfig x11-apps/xhost x11-base/xorg-server x11-libs/libXext x11-libs/libdrm x11-libs/libxcb x11-libs/pixman x11-libs/xcb-util-image x11-libs/xcb-util-renderutil x11-misc/dunst xfce-base/thunar xfce-extra/xarchiver xfce-extra/thunar-archive-plugin x11-misc/rofi x11-misc/stalonetray app-admin/doas x11-misc/cbatticon app-shells/dash lxde-base/lxappearance x11-misc/sddm net-p2p/qbittorrent
+    cd ..
+    git clone https://github.com/aczw/sddm-theme-corners.git
+    cd sddm-theme-corners
+    sudo cp -r corners/ /usr/share/sddm/themes/
+    cd ../DWM
   elif [ "${DISTRO:?}" = "Arch" ] || [ "${DISTRO:?}" = "arch" ]
   then
-    sudo pacman -S git fontconfig xorgproto libx11 libxft libxinerama alacritty ttf-jetbrains-mono ttf-font-awesome feh xorg-xsetroot xorg-setxkbmap network-manager-applet dunst thunar xarchiver thunar-archive-plugin rofi doas cbatticon dash lxappearance sddm qbittorrent > /dev/null
-    yay -S otf-takao volctl stalonetray pnmixer > /dev/null
+    sudo pacman -S git fontconfig xorgproto libx11 libxft libxinerama alacritty ttf-jetbrains-mono ttf-font-awesome feh xorg-xsetroot xorg-setxkbmap network-manager-applet dunst thunar xarchiver thunar-archive-plugin rofi doas cbatticon dash lxappearance sddm qbittorrent
+    yay -S otf-takao volctl stalonetray pnmixer sddm-theme-corners-git
   else
-    sudo add-apt-repository ppa:aslatter/ppa > /dev/null
-    sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable > /dev/null
-    sudo apt install git build-essential dunst thunar rofi nm-tray fonts-takao libxcb-render-util0-dev libxcb-image0-dev libpixman-1-dev libxcb-util-dev libxcb-damage0-dev libxcb-randr0-dev libxcb-sync-dev libxcb-composite0-dev libxcb-xinerama0-dev libxcb-present-dev libxcb-glx0-dev libegl1-mesa-dev libdbus-glib-1-dev libdrm-dev libxext-dev x11-xserver-utils pkg-config libgl-dev dbus ninja-build meson python3-xcffib uthash-dev libpcre3 libpcre3-dev libev-dev libconfig-dev asciidoc python3 gcc pnmixer feh fonts-font-awesome libxinerama-dev libxft-dev libx11-dev fontconfig xorg xserver-xorg x11proto-dev wget libx11-xcb-dev xarchiver thunar-archive-plugin stalonetray doas cbatticon lxappearance sddm alacritty qbittorrent> /dev/null
+    sudo add-apt-repository ppa:aslatter/ppa
+    sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+    sudo apt install git build-essential dunst thunar rofi nm-tray fonts-takao libxcb-render-util0-dev libxcb-image0-dev libpixman-1-dev libxcb-util-dev libxcb-damage0-dev libxcb-randr0-dev libxcb-sync-dev libxcb-composite0-dev libxcb-xinerama0-dev libxcb-present-dev libxcb-glx0-dev libegl1-mesa-dev libdbus-glib-1-dev libdrm-dev libxext-dev x11-xserver-utils pkg-config libgl-dev dbus ninja-build meson python3-xcffib uthash-dev libpcre3 libpcre3-dev libev-dev libconfig-dev asciidoc python3 gcc pnmixer feh fonts-font-awesome libxinerama-dev libxft-dev libx11-dev fontconfig xorg xserver-xorg x11proto-dev wget libx11-xcb-dev xarchiver thunar-archive-plugin stalonetray doas cbatticon lxappearance sddm alacritty qbittorrent
     wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
     unzip JetBrainsMono-2.242.zip
     cd fonts
     sudo mv ttf /usr/share/fonts/jetbrains-mono
     sudo fc-cache -f -v
-    cd ..
+    cd ../..
+    git clone https://github.com/aczw/sddm-theme-corners.git
+    cd sddm-theme-corners
+    sudo cp -r corners/ /usr/share/sddm/themes/
+    cd ../DWM
   fi
 }
 
@@ -90,12 +99,13 @@ config_files_setup() {
   cp stalonetrayrc ~/.config
   ##Configuring qbittorrent
   cd ../qbittorrent
-  mkdir -p ~/.config/qbittorrent
-  cp macchiato.qbtheme ~/.config/qbittorrent
+  mkdir -p ~/.config/qBittorrent/themes
+  cp macchiato.qbtheme ~/.config/qBittorrent/themes
   ##Configuring sddm
   cd ../sddm
   sudo cp sddm.conf /etc/sddm.conf.d
   sudo cp faces/root.face.icon /usr/share/sddm/faces/"${USER:?}".face.icon
+  sudo systemctl enable -f sddm.service
   cd ../..
   ##Configuring dash as default shell for /bin/sh
   sudo ln -sfT dash /bin/sh
